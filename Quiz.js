0f14ee0f3347e57ae0c1c8cab20d96214ad30905
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-06-10 17:47:01 +0530

    Add files via upload

--- Quiz.js
+++ Quiz.js
@@ -7,17 +7,21 @@ var Score2 = 0;
 var Number1;
 var Number2;
 
-var Question;
 var Answer;
+var Decision;
 
+var Question
 var Input;
 var Button;
 var Quiz;
 
+var Ans = "P1";
+var Que = "P2";
 
-function Set(){
-document.getElementById("Player1").innerHTML = Player1 + " : " + Score1;
-document.getElementById("Player2").innerHTML = Player2 + " : " + Score2;
+
+function Display() {
+    document.getElementById("Player1").innerHTML = Player1 + " : " + Score1;
+    document.getElementById("Player2").innerHTML = Player2 + " : " + Score2;
 }
 
 function Show() {
@@ -26,9 +30,37 @@ function Show() {
     Answer = Number1 * Number2;
     Question = " <Center><Br> <H1 class = 'Green-Text'>" + Number1 + " x " + Number2 + "</H1>";
     Input = "<Br><H4 class = Orange-Text>Answer : <Input Id = 'User-Answer' placeholder = 'Enter the Answer here' type = 'number' class = 'set'><H4>";
-    Button = "<br><Button onclick = 'check' class = 'set Yellow-Text'>Check My Answer</Button></Center>";
+    Button = "<br><Button onclick = 'Check()' class = 'set Yellow-Text'>Check My Answer</Button></Center>";
     Quiz = Question + Input + Button;
     document.getElementById("Output").innerHTML = Quiz;
     document.getElementById("Number-1").value = "";
     document.getElementById("Number-2").value = "";
+}
+
+function Check() {
+    Decision = document.getElementById("User-Answer").value;
+
+    if (Decision == Answer) {
+       if( Ans == "P1"){
+        Score1 = Score1 + 1;
+        document.getElementById("Player1").innerHTML = Player1 + " : " + Score1;
+       }
+       else {
+        Score2 = Score2 + 1;
+        document.getElementById("Player2").innerHTML = Player2 + " : " + Score2;}
+
+    }
+
+    if (Ans == "P1"){
+        Ans = "P2";
+        document.getElementById("Type-player-2").innerHTML = "Answer Turn";
+        document.getElementById("Type-player-1").innerHTML = "Question Turn";
+    }
+    else{
+        Ans = "P1";
+        document.getElementById("Type-player-1").innerHTML = "Answer Turn";
+        document.getElementById("Type-player-2").innerHTML = "Question Turn";
+    }
+
+    document.getElementById("Output").innerHTML = "";
 }
